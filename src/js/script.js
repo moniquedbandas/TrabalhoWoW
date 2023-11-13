e9c7fa2a71e150af5d99f12405ad2d5db4ad93d7
moniquebandas <moniquedbandas@gmail.com>
2023-11-13 12:04:48 -0300

    todas as imagens do banner inicial selecionadas.

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     const newLocal = '.banner .slide';
+//     var slides = document.querySelectorAll(newLocal);
+//     var indiceAtual = 0;
+
+//     function exibirProximoSlide() {
+//         slides[indiceAtual].style.display = 'none';
+//         indiceAtual = (indiceAtual + 1) % slides.length;
+//         slides[indiceAtual].style.display = 'block';
+//     }
+
+//     slides[indiceAtual].style.display = 'block';
+
+//     setInterval(exibirProximoSlide, 3000);
+// });
