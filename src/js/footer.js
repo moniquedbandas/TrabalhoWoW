0ccfb95b738027b140c263b6c21f5226a0ed66b5
Monique <moniquedbandas@gmail.com>
2023-11-19 23:06:31 -0300

    footer noticias arrumado

--- src/js/footer.js
+++ src/js/footer.js
@@ -5,9 +5,17 @@ function ajustarFooter() {
     const footer = document.querySelector('footer');
 
     if (alturaConteudo <= alturaJanela) {
-        footer.style.position = 'fixed'; //fixa a posição do footer
-        footer.style.bottom = '0'; //e deixa o footer no final da pagina
-        // footer.style.width = '100%';
+        // footer.style.position = 'fixed'; //fixa a posição do footer
+        // footer.style.bottom = '0'; //e deixa o footer no final da pagina
+        // footer.style.width = '50%';
+        // footer.style.marginLeft = 'auto';
+        // footer.style.marginRight = 'auto';
+        footer.style.position = 'fixed';
+        footer.style.bottom = '0';
+        footer.style.left = '50%'; /* Centraliza o lado esquerdo */
+        footer.style.transform = 'translateX(-50%)'; /* Move 50% para trás */
+        footer.style.width = '50%';
+       
     } else {
         footer.style.position = 'static'; //define a posição do footer como padrão
     }
